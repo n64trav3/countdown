8fad30ded27a806511a75a013db4335e7b69dbda
n64trav3 <tryan@kansascityhomes.com>
2018-08-11 16:31:53 -0400

    Countdown

---
+++ index.php
@@ -0,0 +1,94 @@
+<?php 
+session_start();
+include("library/class_email_sender.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<!-- Go to GENERAL.JS to CHANGE DATE -->
+<head>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>2018</title>
+	
+    <link name="author" href="Faisal Russel(russel365)" />
+    <meta name="description" content="Coming soon">
+    
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="favicon.ico">
+    
+    <!-- Mobile -->
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+    
+	<!-- CSS start here -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="css/styles.css" />
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/animate.css" />
+	<!-- CSS end here -->
+	<!-- Google fonts start here -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
+	<!-- Google fonts end here -->
+</head>
+<body class="ux">
+	<!-- Preloader start here -->
+	<div id="preloader">
+		<div id="status"></div>
+	</div>
+	<!-- Preloader end here -->
+<!-- About Icon start here -->
+		<!-- Slogan end here -->
+		<!-- Count Down start here -->
+		<center><h1>Days 'till HalloWeekends</h1></center>
+		<section class="count-down-wrapper fade-down">
+			<ul class="row count-down">                       
+				<li class="col-md-3 col-sm-6">   
+					<input class="knob days" data-readonly=true data-min="0" data-max="365" data-width="260" data-height="260" data-thickness="0.07" data-fgcolor="#34aadc" data-bgColor="#e1e2e6" data-angleOffset="180">
+					<span id="days-title">days</span>
+				</li>    
+				<li class="col-md-3 col-sm-6"> 
+					<input class="knob hour" data-readonly=true data-min="0" data-max="24" data-width="260" data-height="260" data-thickness="0.07" data-fgcolor="#4cd964" data-bgColor="#e1e2e6" data-angleOffset="180">
+					<span id="hours-title">hours</span>
+				</li>    
+				<li class="col-md-3 col-sm-6"> 
+					<input class="knob minute" data-readonly=true data-min="0" data-max="60" data-width="260" data-height="260" data-thickness="0.07" data-fgcolor="#ff9500" data-bgColor="#e1e2e6" data-angleOffset="180">
+					<span id="mins-title">minutes</span>
+				</li>    
+				<li class="col-md-3 col-sm-6"> 
+					<input class="knob second" data-readonly=true data-min="0" data-max="60" data-width="260" data-height="260" data-thickness="0.07" data-fgcolor="#ff3b30" data-bgColor="#e1e2e6" data-angleOffset="180">
+					<span id="secs-title">seconds</span>
+				</li>                
+			</ul>              
+		<center style="">
+			<img src="img\logo.png" style="height:auto; width:25%; margin:0;">
+			<img src="img\haunt.png" style="height:auto; width:25%; margin:0;">
+			<img src="img\hexed.png" style="height:auto; width:25%; margin:0;">
+		</center>
+		</section>
+		
+	</section>
+<!-- About start here -->
+
+  
+        </div>
+      </div>
+    </div>
+<!-- Contact end here --> 
+		<!-- Main container start here -->
+		<!-- Javascript framework and plugins start here -->
+		<script type="text/javascript" src="js/jquery.js"></script> 
+		<script type="text/javascript" src="js/bootstrap.min.js"></script> 
+		<script src="js/jquery.validate.min.js"></script>
+		<script src="js/modernizr.js"></script> 
+		<script type="text/javascript" src="js/appear.js"></script> 		
+		<script src="js/jquery.knob.js"></script>
+		<script src="js/jquery.ccountdown.js"></script>
+		<script src="js/init.js"></script>
+		<script src="js/general.js"></script>
+		
+<!-- Javascript framework and plugins end here -->
+</body>
+</html>
+
+
